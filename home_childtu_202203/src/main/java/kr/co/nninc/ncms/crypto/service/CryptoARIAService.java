b282dd5b2388e826f716064bc50842f1c58f9ec2
USER <USER@PC94>
2023-07-17 14:55:38 +0900

    Update: ARIA쪽 수정

--- home_childtu_202203/src/main/java/kr/co/nninc/ncms/crypto/service/CryptoARIAService.java
+++ home_childtu_202203/src/main/java/kr/co/nninc/ncms/crypto/service/CryptoARIAService.java
@@ -1,61 +1,59 @@
 package kr.co.nninc.ncms.crypto.service;
 
-
 import java.io.UnsupportedEncodingException;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
 import egovframework.rte.fdl.cryptography.EgovCryptoService;
 import egovframework.rte.fdl.cryptography.EgovPasswordEncoder;
-import kcb.org.apache.commons.codec.binary.Base64;
+//import org.apache.commons.net.util.Base64;
 import kr.co.nninc.ncms.common.CommonConfig;
-import kr.co.nninc.ncms.common.Func;
 
 @Service("cryptoService")
 public class CryptoARIAService {
+	private String plainPassword = "ncmsv2";
 	
 	ApplicationContext context = new ClassPathXmlApplicationContext("egovframework/spring/com/context-ariacrypto.xml");
-	
+
 	private EgovCryptoService cryptoService = (EgovCryptoService) context.getBean("ARIACryptoService");
 	//@Resource(name="ARIACryptoService")
 	//EgovCryptoService cryptoService;
-	
+
 	private EgovPasswordEncoder passwordEncoder = (EgovPasswordEncoder) context.getBean("passwordEncoder");
 	//@Resource(name="passwordEncoder")
 	//private EgovPasswordEncoder passwordEncoder;
-	
-	
+
+
 	public String encryptData(String plainText) throws Exception {
 		String encodeText = null;
-		String plainPassword = "egovframe";
 		String passwdAlgorithm = CommonConfig.get("crypto.password.algorithm");
-		
+	
 		try {
 			byte[] encrypted = cryptoService.encrypt(plainText.getBytes("UTF-8"), plainPassword);
 			encodeText = Base64.encodeBase64String(encrypted);
 		}catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		
-		return encodeText;
+	
+			return encodeText;
 	}
 	
 	public String decryptData(String encodeText) throws Exception {
 		String plainText = null;
-		String plainPassword = "egovframe";
 		String passwdAlgorithm = CommonConfig.get("crypto.password.algorithm");
-		
+	
 		try {
 			byte[] base64dec = Base64.decodeBase64(encodeText);
 			byte[] decrypted = cryptoService.decrypt(base64dec, plainPassword);
-			
+		
 			plainText = new String(decrypted, "UTF-8");
 		}catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		
+	
 		return plainText;
 	}
 }
